9fb0edf2844e0c87567aa279dac77c14a1b04179
giakinh0823 <giakinh2000@gmail.com>
2023-07-18 02:08:55 +0700

    update

---
+++ server/Dto/LogRes/LogResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Dto.Log
+{
+    public class LogResponse
+    {
+        public int LogId { get; set; }
+        public int? JobId { get; set; }
+        public int? UserId { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string? Status { get; set; }
+        public string? Output { get; set; }
+    }
+}
